e1948f86789afeb2cd1df6ce04dea15dd0b6b07e
Torres-Neto <torresneto.tn@gmail.com>
2024-09-30 02:01:27 -0300

    Init repo

---
+++ product-backend/src/main/java/com/abutua/product_backend/resources/HelloController.java
@@ -0,0 +1,15 @@
+package com.abutua.product_backend.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello") 
+    public String getHello() {
+        return "Hello Spring Boot é nois";
+    }
+    
+}
